8b352b9678459f237ca15bc1a4db19a8c3d37133
Jones Vitolo <jsvitolo@gmail.com>
2015-09-06 12:09:01 -0300

    Comments removed

--- menor.rb
+++ menor.rb
@@ -2,7 +2,7 @@ puts "Informe 3 numeros"
 numeros = gets
 
 n = numeros.split.map(&:to_i).sort.uniq
-# n_iguais = n[0] == n[1] &&  n[0] == n[2] && n[1] == n[2]
+
 n_iguais = n.size == 1
 
 menor_maior =  "O menor numero informado é #{n.first} e o maior numero é #{n.last}"
